f1ead99c762b3344d78791507025e2d6060e0af9
LalZzy <1064225490@qq.com>
2020-05-14 17:35:05 +0800

    alter algorithm

--- incremental_learning.py
+++ incremental_learning.py
@@ -23,11 +23,15 @@ def split_tripple(tripple, incre_idx):
     T1 = tripple[bin_mark == 1]
     T2 = tripple[bin_mark == 2]
     T3 = tripple[bin_mark == 4]
+    T4 = tripple[bin_mark == 3]
+    T5 = tripple[bin_mark == 5]
+    T6 = tripple[bin_mark == 6]
+    T7 = tripple[bin_mark == 7]
     T = tripple[bin_mark == 0]
-    return T, T1, T2, T3
+    return T, T1, T2, T3, T4, T5, T6, T7
 
 
-def cal_l0(A,X,st_idx,F,T,T1,T2,T3):
+def cal_l0(A, X, st_idx, F, T, T1, T2, T3):
     row_st_idx, col_st_idx = st_idx
     X1 = X[:row_st_idx, :col_st_idx]
     X2 = X[:row_st_idx, col_st_idx:]
@@ -37,16 +41,16 @@ def cal_l0(A,X,st_idx,F,T,T1,T2,T3):
 
     incre_word_num = A.shape[0] - col_st_idx
     incre_course_num = X.shape[0] - row_st_idx
-    sigma1 = np.zeros((row_st_idx,row_st_idx))
-    l0 = np.zeros((col_st_idx,col_st_idx))
+    sigma1 = np.zeros((row_st_idx, row_st_idx))
+    l0 = np.zeros((col_st_idx, col_st_idx))
     for t in T:
         i, j, k = t
         sigma1[i,j] += max(0, 1 - F[i, j] + F[i, k])
         sigma1[i,k] -= max(0, 1 - F[i, j] + F[i, k])
     l0 -= np.matmul(np.matmul(X1.transpose(), sigma1), X1)
     
-    sigma1 = np.zeros((row_st_idx,row_st_idx))
-    sigma2 = np.zeros((row_st_idx,incre_course_num))
+    sigma1 = np.zeros((row_st_idx, row_st_idx))
+    sigma2 = np.zeros((row_st_idx, incre_course_num))
     for t in T2:
         i, j, k = t
         sigma1[i,k] -= max(0, 1 - F[i, j] + F[i, k])
@@ -69,16 +73,202 @@ def cal_l0(A,X,st_idx,F,T,T1,T2,T3):
         sigma3[i-row_st_idx,k] -= max(0, 1 - F[i, j] + F[i, k])
     l0 += np.matmul(np.matmul(X3.transpose(), sigma3),X1)
     return l0
-    
-        
 
-def incre_cgl_rank(X, st_idx, tripple, T, T1, T2, T3, A0, lamb, eta, tolerrence=0.00001, silence=False, 
+def calc_l1(A, X, st_idx, F, split_tripple_list):
+    row_st_idx, col_st_idx = st_idx
+    p = A.shape[0] - col_st_idx
+    X1 = X[:row_st_idx, :col_st_idx]
+    X2 = X[:row_st_idx, col_st_idx:]
+    X3 = X[row_st_idx:, :col_st_idx]
+    X4 = X[row_st_idx:, col_st_idx:]
+    T,T1,T2,T3,T4,T5,T6,T7 = split_tripple_list
+
+    incre_word_num = A.shape[0] - col_st_idx
+    incre_course_num = X.shape[0] - row_st_idx
+    sigma1 = np.zeros((row_st_idx, incre_course_num))
+    sigma2 = np.zeros((row_st_idx, incre_course_num))
+    l1 = np.zeros((col_st_idx, p))
+    for t in T2:
+        i, j, k = t
+        sigma1[i, j-row_st_idx] += max(0, 1 - F[i, j] + F[i, k])
+        
+    l1 -=  np.matmul(np.matmul(X1.transpose(), sigma1), X4)
+    for t in T3:
+        i, j, k = t
+        sigma2[i, k-row_st_idx] += max(0, 1 - F[i, j] + F[i, k])
+    l1 += np.matmul(np.matmul(X1.transpose(), sigma2), X4)
+    
+    sigma3 = np.zeros((incre_course_num, incre_course_num))
+    for t in T5:
+        i, j, k = t
+        sigma3[i-row_st_idx, k-row_st_idx] += max(0, 1 - F[i, j] + F[i, k])
+    l1 += np.matmul(np.matmul(X3.transpose(), sigma3), X4)
+        
+    sigma4 = np.zeros((row_st_idx, incre_course_num))
+    for t in T6:
+        i, j, k = t
+        sigma4[i, j-row_st_idx] -= max(0, 1 - F[i, j] + F[i, k])
+        sigma4[i, k-row_st_idx] += max(0, 1 - F[i, j] + F[i, k])
+    l1 += np.matmul(np.matmul(X1.transpose(), sigma4), X4)
+    
+    sigma5 = np.zeros((incre_course_num, incre_course_num))
+    for t in T4:
+        i, j, k = t
+        sigma5[i-row_st_idx, j-row_st_idx] -= max(0, 1 - F[i, j] + F[i, k])
+    l1 += np.matmul(np.matmul(X3.transpose(), sigma5), X4)
+    
+    sigma6 = np.zeros((incre_course_num, incre_course_num))
+    for t in T7:
+        i, j, k = t
+        sigma6[i-row_st_idx, j-row_st_idx] -= max(0, 1 - F[i, j] + F[i, k])
+        sigma6[i-row_st_idx, k-row_st_idx] += max(0, 1 - F[i, j] + F[i, k])
+    l1 += np.matmul(np.matmul(X3.transpose(), sigma6), X4)
+    return l1 
+
+def calc_l2(A, X, st_idx, F, split_tripple_list):
+    row_st_idx, col_st_idx = st_idx
+    p = A.shape[0] - col_st_idx
+    X1 = X[:row_st_idx, :col_st_idx]
+    X2 = X[:row_st_idx, col_st_idx:]
+    X3 = X[row_st_idx:, :col_st_idx]
+    X4 = X[row_st_idx:, col_st_idx:]
+    T,T1,T2,T3,T4,T5,T6,T7 = split_tripple_list
+    
+    incre_word_num = A.shape[0] - col_st_idx
+    incre_course_num = X.shape[0] - row_st_idx
+    sigma1 = np.zeros((incre_course_num, row_st_idx))
+    l2 = np.zeros((p, col_st_idx))
+    for t in T1:
+        i, j, k = t
+        sigma1[i-row_st_idx, j] -= max(0, 1 - F[i, j] + F[i, k])
+        sigma1[i-row_st_idx, k] += max(0, 1 - F[i, j] + F[i, k])
+    l2 +=  np.matmul(np.matmul(X4.transpose(), sigma1), X1)
+    
+    sigma2 = np.zeros((incre_course_num, incre_course_num))
+    sigma3 = np.zeros((incre_course_num, row_st_idx))
+    for t in T4:
+        i, j, k = t
+        print(i,j,k)
+        sigma2[i-row_st_idx, j-row_st_idx] -= max(0, 1 - F[i, j] + F[i, k])
+        sigma3[i-row_st_idx, k] += max(0, 1 - F[i, j] + F[i, k])
+    l2 += np.matmul(np.matmul(X4.transpose(), sigma2), X3) + np.matmul(np.matmul(X4.transpose(), sigma3), X1) 
+    
+    sigma4 = np.zeros((incre_course_num, row_st_idx))
+    sigma5 = np.zeros((incre_course_num, incre_course_num))
+    for t in T5:
+        i, j, k = t
+        sigma4[i-row_st_idx, j] -= max(0, 1 - F[i, j] + F[i, k])
+        sigma5[i-row_st_idx, k-row_st_idx] -= max(0, 1 - F[i, j] + F[i, k])
+    l2 += np.matmul(np.matmul(X4.transpose(), sigma4), X1) + np.matmul(np.matmul(X4.transpose(), sigma5), X3)
+       
+    sigma6 = np.zeros((incre_course_num, incre_course_num))
+    for t in T7:
+        i, j, k = t
+        sigma6[i-row_st_idx, j-row_st_idx] -= max(0, 1 - F[i, j] + F[i, k])
+        sigma6[i-row_st_idx, k-row_st_idx] += max(0, 1 - F[i, j] + F[i, k])
+    l2 += np.matmul(np.matmul(X4.transpose(), sigma6), X3)
+    return l2
+
+    
+def incre_cgl_rank_new(X, st_idx, tripple, split_tripple_list, A0, lamb, eta, tolerrence=0.00001, silence=False, 
                    update_A1=False):
     '''
     增量学习版本，可以支持一门课多个词的输入。
     '''
     # TODO: 这里需要改
     # X1~4分别表示分块矩阵的四块，按行编号。
+    st = datetime.now()
+    A = A0.copy()
+    T,T1,T2,T3,T4,T5,T6,T7 = split_tripple_list
+    row_st_idx, col_st_idx = st_idx
+    X1 = X[:row_st_idx, :col_st_idx]
+    X2 = X[:row_st_idx, col_st_idx:]
+    X3 = X[row_st_idx:, :col_st_idx]
+    X4 = X[row_st_idx:, col_st_idx:]
+    incre_course_num = X.shape[0] - row_st_idx
+    F = np.matmul(np.matmul(X, A), X.transpose())
+
+    # loss function: max( (1-F_ij+F_ik),0 ) square.
+    def loss_func(x): return max(
+        (1 - F[x[0], x[1]] + F[x[0], x[2]]), 0)**2
+    
+    round_A2 = 0
+    eta1 = eta
+    old_loss = np.inf
+    while True:
+        F = np.matmul(np.matmul(X, A), X.transpose())
+        p = A.shape[0] - col_st_idx
+        # l1,l2的形状跟待更新的A2,A3一样
+        l1 = calc_l1(A, X, st_idx, F, split_tripple_list)
+        l2 = calc_l2(A, X, st_idx, F, split_tripple_list)
+        
+        if update_A1:
+            l0 = cal_l0(A, X, st_idx, F, T, T1, T2, T3)
+        # A1 after this round update.
+        while True:
+            F_old = F.copy()
+            A_old = A.copy()
+            A[:col_st_idx, col_st_idx:] -= eta1 * (lamb * A[:col_st_idx, col_st_idx:] + l1)
+            A[col_st_idx:, :col_st_idx] -= eta1 * (lamb * A[col_st_idx:, :col_st_idx] + l2)
+            if update_A1:
+                A[:col_st_idx, :col_st_idx] -= eta1 * (lamb * A[:col_st_idx, :col_st_idx] + l0)
+            F = np.matmul(np.matmul(X, A), X.transpose())
+            
+            total1 = 0
+            correct1 = 0
+            for i, j, k in T2:
+                if F[i,j] - F[i,k] > 0:
+                    correct1 += 1
+                total1 += 1
+            
+            total2 = 0
+            correct2 = 0
+            for i,j,k in T3:
+                if F[i,j] - F[i,k] > 0:
+                    correct2 += 1
+                total2 += 1
+                    
+            loss_part = sum(list(map(loss_func, T2))) + sum(list(map(loss_func, T3)))
+            if update_A1:
+                loss_part += sum(list(map(loss_func, T))) 
+            reg_part = lamb/2 * np.sqrt((A[:col_st_idx, col_st_idx:]**2).sum())
+            cur_loss = loss_part + reg_part
+            unit_loss_change = (old_loss - cur_loss) / A[:col_st_idx, col_st_idx:].size
+            loss_change = (old_loss - cur_loss)
+        
+            if unit_loss_change < 0:
+                eta1 *= 0.95
+                F = F_old.copy()
+                A = A_old.copy()
+            else:
+                break
+            
+        if not silence:
+            if round_A2 % 10 == 0:
+                print('num update A2: {}, eta: {}, loss decrease: {}'.format(round_A2, eta1, loss_change))
+                print('loss_part={}, reg_part={}'.format(loss_part, reg_part))
+                print('loss_part in tripple:{}'.format(sum(list(map(loss_func, tripple)))))
+                if total1 and total2:
+                    print('correct ratio T2:{}, T3:{}'.format(correct1/total1, correct2/total2))
+                print('loss part T2:{}, T3:{}'.format(sum(list(map(loss_func, T2))),sum(list(map(loss_func, T3)))))
+        round_A2 += 1
+        if loss_change < tolerrence:
+            break
+        old_loss = cur_loss
+    print('train A2 cost: {} step, final loss: {}, loss_change: {}'.format(round_A2, cur_loss, loss_change))
+    F = np.matmul(np.matmul(X, A), X.transpose())
+    ed = datetime.now()
+    return A, F, (ed-st).total_seconds()
+        
+
+def incre_cgl_rank(X, st_idx, tripple, split_tripple_list, A0, lamb, eta, tolerrence=0.00001, silence=False, 
+                   update_A1=False):
+    '''
+    增量学习版本，可以支持一门课多个词的输入。
+    '''
+    # TODO: 这里需要改
+    # X1~4分别表示分块矩阵的四块，按行编号。
+    T, T1, T2, T3, T4, T5, T6, T7 = split_tripple_list
     st = datetime.now()
     A = A0.copy()
     row_st_idx, col_st_idx = st_idx
@@ -101,8 +291,8 @@ def incre_cgl_rank(X, st_idx, tripple, T, T1, T2, T3, A0, lamb, eta, tolerrence=
         p = A.shape[0] - col_st_idx
         # l1,l2的形状跟待更新的A2,A3一样
         l1, l2 = np.zeros((col_st_idx, p)), np.zeros((col_st_idx, p))
-        sigma1 = np.zeros((row_st_idx,incre_course_num))
-        sigma2 = np.zeros((row_st_idx,incre_course_num))
+        sigma1 = np.zeros((row_st_idx, incre_course_num))
+        sigma2 = np.zeros((row_st_idx, incre_course_num))
         for t in T2:
             i, j, k = t
             sigma1[i,j-row_st_idx] += max(0, 1 - F[i, j] + F[i, k])
@@ -110,7 +300,7 @@ def incre_cgl_rank(X, st_idx, tripple, T, T1, T2, T3, A0, lamb, eta, tolerrence=
         l1 +=  np.matmul(np.matmul(X1.transpose(), sigma1), X4)
         for t in T3:
             i, j, k = t
-            sigma2[i,k-row_st_idx] += max(0, 1 - F[i, j] + F[i, k])
+            sigma2[i, k-row_st_idx] += max(0, 1 - F[i, j] + F[i, k])
         l2 += np.matmul(np.matmul(X1.transpose(), sigma2), X4)
         
         if update_A1:
@@ -226,13 +416,13 @@ def test_incre(data_file, link_file, concept_file, file_type, incre_course_num, 
     n_course, n_concept = X.shape[0], X.shape[1]
     trn = generate_trn(links, n_course, undirect=undirect)
     tripple = generate_triple(trn)
-    T, T1, T2, T3 = split_tripple(tripple, range(X.shape[0]-incre_course_num,X.shape[0]))
+    split_tripple_list = split_tripple(tripple, range(X.shape[0]-incre_course_num,X.shape[0]))
     # 找到一个只有最后一门课才有的词
     X = row_normlize(X)
     # 先训不带增量最后一行的
     # 用全量数据训练模型
     import model
-    A0, F0 = model.cgl_rank(X, tripple, lamb=0.01, eta=1,
+    A0, F0, st = model.cgl_rank(X, tripple, lamb=0.01, eta=1,
                           tolerence=0.001, silence=False)
     print('finish training whole A\n\n')
     # 用增量数据训练模型
@@ -241,43 +431,46 @@ def test_incre(data_file, link_file, concept_file, file_type, incre_course_num, 
     
     A = gene_incre_matrix(A, incre_concept_num)
     print('\n\n\n')
-    A1, F1 = incre_cgl_rank(X, (n_course-incre_course_num, n_concept-incre_concept_num), T, T1, T2, T3, A, eta=5, lamb=0.01,
+    A1, F1, st = incre_cgl_rank(X, (n_course-incre_course_num, n_concept-incre_concept_num), tripple, split_tripple_list, A, eta=5, lamb=0.01,
                         tolerrence=0.001, update_A1=update_A1)
     file_prefix = 'undirect' if undirect else 'direct'
     file_prefix += '_update_A1' if update_A1 else 'noupdate_A1'
     if save:
-        np.savetxt('result/ruc_A_whole_{}.txt'.format(file_prefix), A0)
-        np.savetxt('result/ruc_A_incre_{}.txt'.format(file_prefix), A1)
+        np.savetxt('result/ruc_A_whole_with_essay_{}.txt'.format(file_prefix), A0)
+        np.savetxt('result/ruc_A_incre_with_essay_{}.txt'.format(file_prefix), A1)
 
-        np.savetxt('result/ruc_F_whole_{}.txt'.format(file_prefix), F0)
-        np.savetxt('result/ruc_F_incre_{}.txt'.format(file_prefix), F1)
+        np.savetxt('result/ruc_F_whole_with_essay_{}.txt'.format(file_prefix), F0)
+        np.savetxt('result/ruc_F_incre_with_essay_{}.txt'.format(file_prefix), F1)
     
 
 
 def test_split_tripple():
-    test_links = np.array([[1, 2], [1, 3], [2, 4]])
+    test_links = np.array([[0, 2], [0, 3], [1, 2]])
     from preprocessing import generate_triple, generate_trn
     trn = generate_trn(test_links, 4)
     tripple = generate_triple(trn)
-    test_links = np.array([[1, 2], [1, 3], [2, 4], [1, 5], [5, 2], [6, 1]])
+    test_links = np.array([[1, 2], [1, 3], [2, 4], [1, 5], [5, 2], [6, 1], [5, 6]])
     tripple_more = generate_triple(generate_trn(test_links, 7))
     #import pdb;pdb.set_trace()
     print(tripple)
     print('after add 5,tripple become: {}\nlength:{}'.format(
         tripple_more, len(tripple_more)))
-    T, T1, T2, T3 = split_tripple(tripple_more, [5, 6])
+    T, T1, T2, T3, T4, T5, T6, T7 = split_tripple(tripple_more, [5, 6])
     print('origin T:\n{}\nincre as pre:\n{}\nincre as post:\n{}\nincre post nor pre:\n{}\n'.format(
         T, T1, T2, T3))
+    print(T4,T5,T6,T7)
 
 
 if __name__ == '__main__':
     # test_split_tripple()
-    data_file = 'data/all_ruc_word_info_new.csv'
-    link_file = 'data/all_ruc_new_link.csv'
-    concept_file = 'data/ruc_all_concepts_new.csv'
+    
+    data_file = 'data/all_ruc_word_info_new_with_essay.csv'
+    link_file = 'data/all_ruc_new_link_with_essay.csv'
+    concept_file = 'data/ruc_all_concepts_new_with_essay.csv'
     file_type = 'sparse_row_col_val'
-    incre_course_num = 3
-    incre_concept_num = 18
+    incre_course_num = 176
+    incre_concept_num = 149
     test_incre(data_file, link_file, concept_file, file_type, incre_course_num, incre_concept_num, save=True)
-    test_incre(data_file, link_file, concept_file, file_type, incre_course_num, incre_concept_num, save=True, update_A1=True)
-    test_incre(data_file, link_file, concept_file, file_type, incre_course_num, incre_concept_num, undirect=True, save=True)
+    # test_incre(data_file, link_file, concept_file, file_type, incre_course_num, incre_concept_num, save=True, update_A1=True)
+    # test_incre(data_file, link_file, concept_file, file_type, incre_course_num, incre_concept_num, undirect=True, save=True)
+    
